c8ab1de1eba94a2e5a4c01c1b9896e77f4e0f221
shaik bashiruddin <bashiruddin.sk4664@gmail.com>
2024-08-10 10:03:59 +0530

    youtube

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TrendingComponent } from './trending/trending.component';
+import { SubscriptionsComponent } from './subscriptions/subscriptions.component';
+import { HistoryComponent } from './history/history.component';
+import { WatchLaterComponent } from './watch-later/watch-later.component';
+import { LikedVideosComponent } from './liked-videos/liked-videos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'trending', component: TrendingComponent },
+  { path: 'subscriptions', component: SubscriptionsComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: 'watch-later', component: WatchLaterComponent },
+  { path: 'liked-videos', component: LikedVideosComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
+  { path: '**', redirectTo: '/home' } // Wildcard route for a 404 page, if needed
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { TrendingComponent } from './trending/trending.component';
+import { SubscriptionsComponent } from './subscriptions/subscriptions.component';
+import { HistoryComponent } from './history/history.component';
+import { WatchLaterComponent } from './watch-later/watch-later.component';
+import { LikedVideosComponent } from './liked-videos/liked-videos.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    TrendingComponent,
+    SubscriptionsComponent,
+    HistoryComponent,
+    WatchLaterComponent,
+    LikedVideosComponent,
+    NavbarComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
+    
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
